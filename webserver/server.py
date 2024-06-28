3f3f9f4cd8dfcd9e1619a5d2bc76634a3f7cd75b
Thbop <75861321+thbop@users.noreply.github.com>
2024-06-28 18:30:48 -0400

    Added some threading for multiple connections

--- webserver/server.py
+++ webserver/server.py
@@ -1,18 +1,30 @@
-import asyncio
 import socket
+import threading
+
+def connection(conn, addr):
+    print(f'Connection received from {addr[0]}:{addr[1]}')
+    while True:
+        data = conn.recv(1024)
+        conn.send(b'Hello there!')
+        if not data: break
+        print(data.decode())
+    conn.close()
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 4242))
-print('Listening')
-sock.listen()
-conn, conn_addr = sock.accept()
-print(f'Connection received from {conn_addr[0]}:{conn_addr[1]}')
-while True:
-    data = conn.recv(1024)
-    conn.send(b'Hello there!')
-    if not data: break
-    print(data.decode())
-conn.close()
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 4242))
 
-sock.close()
+    print('Listening')
+
+    while True:
+        sock.listen()
+        conn, conn_addr = sock.accept()
+        threading.Thread(target=connection,args=(conn, conn_addr), daemon=True).start() 
+        
+
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
